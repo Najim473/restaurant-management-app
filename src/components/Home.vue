4f8f33d48bec9848911e2776febf223b16fe840d
Najim Uddin <najim7304@gmail.com>
2023-01-02 13:43:04 +0600

    vue js router successfully done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,19 @@
-<tempplate>
- <div>
- <h3>
- This HOme page
- </h3>
- </div>
-</tempplate>
- <script>
-//  eslint-disable-next-line
- /* eslint-disable */
- export default{
+<template>
+    <p>This is Home page</p>
+</template>
+<script >
+// eslint-disable-next-line
+/* eslint-disable */
+export default{
     name:"Home"
- }
+}
 </script>
+<style>
+p{
+    background: #000;
+    color: #fff;
+    font-size: 30px;
+    padding: 50px;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+</style>
