e87936443e3d461bef5a570bbb43f7eca59a3ab1
Najim Uddin <najim7304@gmail.com>
2023-01-02 23:38:20 +0600

    deleted function successfully done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -25,7 +25,10 @@
         </td>
         <!-- <td><router-link to="/update">update</router-link></td> -->
         <!-- For catch id  -->
-        <td><router-link :to="'/update/'+item.id">update</router-link></td>
+        <td>
+          <router-link :to="'/update/' + item.id">update</router-link>
+          <button @click="deleteRestaurant(item.id)">Delete</button>
+        </td>
       </tr>
     </table>
   </div>
@@ -42,19 +45,42 @@ export default {
       restaurant: [],
     };
   },
+  methods: {
+    async deleteRestaurant(id) {
+      console.log(id);
+      let result = await axios.delete("http://localhost:3000/restaurant/" + id);
+      console.log(result);
+      if (result.status == 200) {
+        this.loadData();
+      }
+    },
+    async loadData() {
+      let user = localStorage.getItem("user-info");
+      this.name = JSON.parse(user).name;
+      if (!user) {
+        this.$router.push({ name: "SignUp" });
+      }
+      let result = await axios.get("http://localhost:3000/restaurant");
+      console.warn(result);
+      this.restaurant = result.data;
+    },
+  },
   components: {
     Header,
   },
-  async mounted() {
-    let user = localStorage.getItem("user-info");
-    this.name = JSON.parse(user).name;
-    if (!user) {
-      this.$router.push({ name: "SignUp" });
-    }
-    let result = await axios.get("http://localhost:3000/restaurant");
-    console.warn(result);
-    this.restaurant = result.data;
-  },
+  async mounted(){
+    this.loadData();
+  }
+  // async mounted() {
+  //   let user = localStorage.getItem("user-info");
+  //   this.name = JSON.parse(user).name;
+  //   if (!user) {
+  //     this.$router.push({ name: "SignUp" });
+  //   }
+  //   let result = await axios.get("http://localhost:3000/restaurant");
+  //   console.warn(result);
+  //   this.restaurant = result.data;
+  // },
 };
 </script>
 <style lang="css" scoped>
@@ -68,5 +94,14 @@ td {
 td a {
   text-decoration: none;
   font-size: 15px;
+}
+button {
+  border: none;
+  border-left: 1px solid #333;
+  background: none;
+  color: red;
+  cursor: pointer;
+  padding: 5px;
+  margin: 5px;
 }
 </style>
